30c2656566f19585c3d08be19c705a2abc20cfa1
bakedmochocho <oyatyo.kma.27@gmail.com>
2026-02-18 15:12:32 +0900

    first commit.

---
+++ app.py
@@ -0,0 +1,74 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+
+##-----------------------------------------------------------
+
+from langchain_openai import ChatOpenAI
+from langchain.schema import SystemMessage, HumanMessage, AIMessage
+
+llm = ChatOpenAI(model_name="gpt-4o-mini", temperature=0)
+
+##-----------------------------------------------------------
+
+import streamlit as st
+
+def ai_bot(selected_item):
+    if selected_item == "数学の専門家":
+        label_genre = "私は" + selected_item + "です。数学について何でも聞いてください。"
+        input_message = st.text_input(label=label_genre)
+        system_message = "あなたは" + label_genre + "です。入力されてくる質問に答えてください。"
+
+        messages = [
+        SystemMessage(content=system_message),
+        HumanMessage(content=input_message)
+        ]
+
+        if st.button("送信"):
+            if input_message:
+                result = llm(messages)
+                return result  .content
+            else:
+                st.error("質問するテキストを入力してから「送信」ボタンを押してください。")
+    else:
+        label_genre = "私は" + selected_item + "です。医療について何でも聞いてください。"
+        input_message = st.text_input(label=label_genre)
+        system_message = "あなたは" + label_genre + "です。入力されてくる質問に答えてください。"
+
+        messages = [
+        SystemMessage(content=system_message),
+        HumanMessage(content=input_message)
+        ]
+
+        if st.button("送信"):
+            if input_message:
+                result = llm(messages)
+                st.write(result.content)
+                return result.content
+            else:
+                st.error("質問するテキストを入力してから「送信」ボタンを押してください。")
+
+st.title("Q＆A AIチャットボット")
+
+st.write("##### 動作モード1: 数学の専門家")
+st.write("入力フォームにテキストを入力し、「送信」ボタンを押すことで数学の専門家に質問ができます。")
+st.write("##### 動作モード2: 医療の専門家")
+st.write("入力フォームにテキストを入力し、「送信」ボタンを押すことで医療の専門家に質問ができます。")
+
+selected_item = st.radio(
+    "動作モードを選択してください。",
+    ["数学の専門家", "医療の専門家"]
+)
+
+st.divider()
+
+if selected_item == "数学の専門家":
+    result = ai_bot("数学の専門家")
+else:
+    result = ai_bot("医療の専門家")
+
+if(result):
+    st.write(result)
+else:
+    st.write("質問を入力して「送信」ボタンを押してください。")
+
